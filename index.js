2cbe2c2a55a926cb5a6c03d1544d93d293ffd2a7
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2022-12-02 23:33:51 +0000

    qaa

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+var http = require('express');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+var app = http()
+
+app.get('/', async (req,res) => {
+ // store something
+await s3.putObject({
+    Body: JSON.stringify({key:"value"}),
+    Bucket: "cyclic-gold-gentle-mackerel-ap-southeast-2",
+    Key: "my_file.json",
+}).promise()
+
+// get it back
+let my_file = await s3.getObject({
+    Bucket: "cyclic-gold-gentle-mackerel-ap-southeast-2",
+    Key: "my_file.json",
+}).promise()
+
+res.json(JSON.parse(my_file))
+
+})
+app.listen(process.env.PORT || 3000);
