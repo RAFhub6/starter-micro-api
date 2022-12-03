63f93a06e1301dfc10fac29526c733bfe2183c82
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2022-12-03 01:57:56 +0000

    ea

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
 var app = http()
 
-app.get('/', async (req,res) => {
-// get it back
+app.get('/file/:file', async (req,res) => {
+try {
+    // get it back
 let my_file = await s3.getObject({
     Bucket: "cyclic-gold-gentle-mackerel-ap-southeast-2",
-    Key: "my_file.json",
+    Key: req.params('file'),
 }).promise()
-
 res.send(my_file.Body)
-
+} catch(err) {
+ console.log(err)
+ res.send("Error")
+}
 })
 app.listen(process.env.PORT || 3000);
