13a0f1bc182db1c1b9e3cfaeadfd21fcf03d6f3d
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2022-12-03 00:06:21 +0000

    JUST WHY WHY WHY ERRORS

--- index.js
+++ index.js
@@ -4,20 +4,13 @@ const s3 = new AWS.S3()
 var app = http()
 
 app.get('/', async (req,res) => {
- // store something
-await s3.putObject({
-    Body: JSON.stringify({key:"valuse"}),
-    Bucket: "cyclic-gold-gentle-mackerel-ap-southeast-2",
-    Key: "my_file.json",
-}).promise()
-
 // get it back
 let my_file = await s3.getObject({
     Bucket: "cyclic-gold-gentle-mackerel-ap-southeast-2",
     Key: "my_file.json",
 }).promise()
 
-res.json(JSON.parse(my_file))
+res.send(my_file)
 
 })
 app.listen(process.env.PORT || 3000);
